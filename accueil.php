00ac4e2438b35d33c3a22355f9eae285a63c984d
Unknown <vincent.laisne@laposte.net>
2018-11-16 21:48:21 +0100

    Ajout html

---
+++ accueil.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="accueilCSS.css">
+        <title>Passion Loutrons</title>
+    </head>
+
+    <body>
+
+        <header>
+            <div id = "divTitre"> 
+                <img src="images/loutre_tete.jpg" height = "50" width = "50"> 
+                <h2>Passion Loutrons</h2>
+                <img src="images/loutre_tete.jpg" height = "50" width = "50"> 
+            </div>
+            <div id = "divMenu">
+                <h3>Produits</h3>
+                <h3>Vétérinaires</h3>
+                <h3>S'inscrire</h3>
+                <h3>Se connecter</h3>
+            </div>
+        </header>
+
+        <main>
+            <article>
+                <div class="ligne_produit">
+                    <div  class="produits">
+                        <img src="images/loutre_tete.jpg" height="100" width="100">
+                        <h3>Produit 1</h3>
+                    </div>
+                    <div  class="produits">
+                        <img src="images/loutre_tete.jpg" height="100" width="100">
+                        <h3>Produit 2</h3>
+                    </div>
+                    <div  class="produits">
+                        <img src="images/loutre_tete.jpg" height="100" width="100">
+                        <h3>Produit 3</h3>
+                    </div>
+                    <div  class="produits">
+                        <img src="images/loutre_tete.jpg" height="100" width="100">
+                        <h3>Produit 4</h3>
+                    </div>
+                </div>
+                <div class ="ligne_produit">
+                    <div  class="produits">
+                        <img src="images/loutre_tete.jpg" height="100" width="100">
+                        <h3>Produit 5</h3>
+                    </div>
+                    <div  class="produits">
+                        <img src="images/loutre_tete.jpg" height="100" width="100">
+                        <h3>Produit 6</h3>
+                    </div>
+                    <div  class="produits">
+                        <img src="images/loutre_tete.jpg" height="100" width="100">
+                        <h3>Produit 7</h3>
+                    </div>
+                    <div  class="produits">
+                        <img src="images/loutre_tete.jpg" height="100" width="100">
+                        <h3>Produit 8</h3>
+                    </div>
+                </div>
+
+            </article>
+
+            <aside>
+                <img src="images/loutre.jpg" alt="loutres" id="loutre_aside">
+            </aside>
+        </main>
+
+
+        <footer>
+            <p>Credits</p>
+        </footer>
+    </body>
+</html>
